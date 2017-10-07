cb59431f69442ca99a03a88dd346c1c6c9ee53f7
frigidrain <frigidrain@gmail.com>
2017-10-07 22:52:18 +0000

    Make symlinks again.

--- install.sh
+++ install.sh
@@ -12,42 +12,19 @@ if [ ! -d $dotfiles ]; then
   exit 1
 fi
 
-# Files to process
+# Files requiring symlinks
 files="tmux.conf jshintrc"
 
 echo "Creating $backup for backup of any existing dotfiles"
 mkdir -p $backup
 
-ask_for_file() {
-  file=$1
-  read -p "What to do with .$file? [r: replace, a: append, s: skip] " -n 1 -r
-  echo
-  case $REPLY in
-      [Rr]* )
-        echo "Backing up $HOME/.$file in $backup"
-        mv $HOME/.$file $backup
-        echo "Replacing $HOME/.$file"
-        cp $dotfiles/$file $HOME/.$file
-        return 0;;
-      [Aa]* )
-        echo "Backing up $HOME/.$file in $backup"
-        cp $HOME/.$file $backup
-        echo "Appending to $HOME/.$file"
-        cat "$dotfiles/$file" >> "$HOME/.$file"
-        return 0;;
-      [Ss]* ) return 0;;
-      * )
-        echo "Invalid option. (r: replace, a: append, n: skip)"
-        return 1;;
-  esac
-}
-
 for file in $files; do
-  while true; do
-    if ask_for_file $file; then
-      break
-    fi
-  done
+  if ask "Make symlink for .$file?"; then
+    echo "Backing up $HOME/.$file in $backup"
+    mv $HOME/.$file $backup
+    echo "Creating symlink $HOME/.$file ->  $dotfiles/$file"
+    ln -s $dotfiles/$file $HOME/.$file
+  fi
 done
 
 if ask "source $dotfiles/bashrc in your ~/.bashrc?"; then
