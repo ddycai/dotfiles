52b1fb0a76dfbf007a5c4e4b3cc171d8c3a5a141
Duncan <ddycai@gmail.com>
2019-12-17 16:08:26 -0500

    Simplify install script.

--- install.sh
+++ install.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
-# Imports the dotfiles from the repo into your home dir.
+# Sources config files from dotfiles repo.
 
 DIR="${BASH_SOURCE%/*}"
 . "$DIR/common.sh"
 
 dotfiles=$HOME/dotfiles
-backup=$HOME/dotfiles_old
 
 if [ ! -d $dotfiles ]; then
   echo "Please clone the repo into $HOME."
   exit 1
 fi
-
-# Files requiring symlinks
-files="tmux.conf jshintrc"
-
-echo "Creating $backup for backup of any existing dotfiles"
-mkdir -p $backup
-
-for file in $files; do
-  if ask "Make symlink for .$file?"; then
-    echo "Backing up $HOME/.$file in $backup"
-    mv $HOME/.$file $backup
-    echo "Creating symlink $HOME/.$file ->  $dotfiles/$file"
-    ln -s $dotfiles/$file $HOME/.$file
-  fi
-done
 
 if ask "source $dotfiles/bashrc in your ~/.bashrc?"; then
   echo "source \$HOME/dotfiles/bashrc" >> "$HOME/.bashrc"
@@ -33,6 +17,10 @@ fi
 
 if ask "source $dotfiles/vimrc in your ~/.vimrc?"; then
   echo "source \$HOME/dotfiles/vimrc" >> "$HOME/.vimrc"
+fi
+
+if ask "source $dofiles/tmux.conf in your ~/.tmux.conf"; then
+  echo "source \$HOME/dotfiles/tmux.conf" >> "$HOME/.tmux.conf"
 fi
 
 if ask "make symlink for tmux themes in ~/.tmux/themes?"; then
